aa4c228b7d4bc5a36c4626830eec7963d68f6588
kokyaw <kokyaw264.mm@gmail.com>
2024-05-30 09:35:29 +0700

    testing
    testing

--- src/app/component/cimb-lite-test/cimb-lite-test.component.ts
+++ src/app/component/cimb-lite-test/cimb-lite-test.component.ts
@@ -3,6 +3,7 @@ import { APP_IMPORTS } from '../../app.import';
 import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-cimb-lite-test',
@@ -23,7 +24,7 @@ export class CimbLiteTestComponent {
   }> = this.fb.group({
     userName: ['', [Validators.required]],
     date: [new Date(), [Validators.required]],
-    selectVal:['', [Validators.required]]
+    selectVal: ['', [Validators.required]]
   });
 
   submitForm(): void {
@@ -84,4 +85,55 @@ export class CimbLiteTestComponent {
   //radio
   radioValue = '';
   radioValue2 = '1';
+
+  selectedSOW: string[] = [];
+  sWealths = [
+    { name: 'Business proceeds', check: true },
+    { name: 'Earnings from other businesses', check: false },
+    { name: 'Accumulated personal savings', check: false },
+    { name: 'Earnings from previous or current employment', check: false },
+    { name: 'Others', check: false }
+  ];
+
+  clickCheck(indexVal: number) {
+    console.log('indexVal ', indexVal);
+    this.sWealths[indexVal].check = !this.sWealths[indexVal].check;
+    console.log('sWealths', this.sWealths[indexVal]);
+  }
+
+  testClick(val: any): void {
+    if (val == 1) {
+      this.sWealths = [
+        { name: 'Business proceeds', check: true },
+        { name: 'Earnings from other businesses', check: false },
+        { name: 'Accumulated personal savings', check: false },
+        { name: 'Earnings from previous or current employment', check: false },
+        { name: 'Others', check: false }
+      ]
+    } else if (val == 2) {
+      this.sWealths = [
+        { name: 'Business 1', check: true },
+        { name: 'Business 2', check: true },
+        { name: 'Business 3', check: false }
+      ]
+    }else if (val == 3) {
+      this.sWealths = [
+        { name: 'Earnings from other businesses 1', check: false },
+        { name: 'Earnings from other businesses 2', check: false },
+        { name: 'Earnings from other businesses 3', check: true },
+        { name: 'Earnings from other businesses 4', check: false },
+      ]
+    }
+  }
+  log(value: string[]): void {
+    console.log(value);
+  }
+
+  test2=[
+    { name: 'Business proceeds', check: true },
+    { name: 'Earnings from other businesses', check: false },
+    { name: 'Accumulated personal savings', check: false },
+    { name: 'Earnings from previous or current employment', check: false },
+    { name: 'Others', check: false }
+  ]
 }
